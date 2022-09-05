ae83a2614300cba59e045670f7009ca0826bef99
lmsyyds <1298082329@qq.com>
2022-09-05 20:19:32 +0800

    add java code

--- java/java_test/src/App.java
+++ java/java_test/src/App.java
@@ -1,5 +1,37 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+      int[] array = {1,3,5};
+      rev(array);
+      System.out.print("数组的反序：");
+      printArray(array);
+      int[] copy = copy(array);
+      array[0] = 9;
+      System.out.print("修改原数组：");
+      printArray(array);
+      System.out.print("复制的数组：");
+      printArray(copy);
+    }
+
+    public static void rev(int[] pa){
+        for(int i = 0, j = pa.length - 1; i < j; i++, j--){
+            int temp = pa[i];
+            pa[i] = pa[j];
+            pa[j] = temp;
+        }
+    }
+
+    public static int[] copy(int[] pa){
+        int[] newarray = new int[pa.length];
+        for(int i = 0; i < pa.length; i++){
+            newarray[i] = pa[i];
+        }
+        return newarray;
+    }
+
+    public static void printArray(int[] pa){
+        for(int i = 0; i < pa.length; i++){
+            System.out.print(pa[i] + "\t");
+        }
+        System.out.println();
     }
 }
